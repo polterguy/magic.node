1349421a303c546b8df203b533b921b661e7e41c
Thomas Hansen <thomas@gaiasoul.com>
2020-01-16 16:26:26 +0200

    Update build.cmd

--- build.cmd
+++ build.cmd
@@ -1,4 +1,4 @@
 cd %~dp0
-dotnet build magic.node/magic.node.csproj --configuration Release
-dotnet build magic.node.expressions/magic.node.expressions.csproj --configuration Release
-dotnet build magic.node.extensions/magic.node.extensions.csproj --configuration Release
+dotnet build magic.node/magic.node.csproj --configuration Release --source https://api.nuget.org/v3/index.json
+dotnet build magic.node.expressions/magic.node.expressions.csproj --configuration Release --source https://api.nuget.org/v3/index.json
+dotnet build magic.node.extensions/magic.node.extensions.csproj --configuration Release --source https://api.nuget.org/v3/index.json
