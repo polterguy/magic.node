87ffe3f080ca2e7c2d61a5823a9e0645e72f16d5
Thomas Hansen <thomas@gaiasoul.com>
2021-12-21 12:59:14 +0200

    Implementing IMagicConfiguration

---
+++ magic.node.extensions/contracts/IMagicConfiguration.cs
@@ -0,0 +1,18 @@
+﻿/*
+ * Magic Cloud, copyright Aista, Ltd. See the attached LICENSE file for details.
+ */
+
+namespace magic.node.contracts
+{
+    /// <summary>
+    /// Interface wrapping configuration settings for Magic.
+    /// </summary>
+    public interface IMagicConfiguration
+    {
+        /// <summary>
+        /// Returns the specified key as a string value.
+        /// </summary>
+        /// <value></value>
+        string this [string key] { get; set; }
+    }
+}
--- magic.node.extensions/contracts/IRootResolver.cs
+++ magic.node.extensions/contracts/IRootResolver.cs
@@ -2,8 +2,6 @@
  * Magic Cloud, copyright Aista, Ltd. See the attached LICENSE file for details.
  */
 
-using System.Text;
-
 namespace magic.node.contracts
 {
     /// <summary>
@@ -15,6 +13,11 @@ namespace magic.node.contracts
         /// Returns the root folder that magic.lambda.io should treat as the root folder for its IO operations.
         /// </summary>
         string RootFolder { get; }
+
+        /// <summary>
+        /// Returns the root folder that magic.lambda.io should treat as the absolute root folder for its IO operations.
+        /// </summary>
+        string AbsoluteRootFolder { get; }
 
         /// <summary>
         /// Returns the relative path given the absolute path as an argument.
@@ -29,5 +32,12 @@ namespace magic.node.contracts
         /// <param name="path">Relative path of file or folder.</param>
         /// <returns>Absolute file or folder path.</returns>
         string AbsolutePath(string path);
+
+        /// <summary>
+        /// Returns the absolute root path given the relative path as an argument.
+        /// </summary>
+        /// <param name="path">Relative path of file or folder.</param>
+        /// <returns>Absolute root file or folder path.</returns>
+        string RootPath(string path);
     }
 }
